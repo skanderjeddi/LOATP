863fff0ed4e0a9258e5fb551fde5e5e68c9487f6
Skander Jeddi <skander.jeddi2@gmail.com>
2022-10-27 16:30:22 +0200

    Changed formatting.

--- main.cpp
+++ main.cpp
@@ -5,10 +5,7 @@ bool DEBUG;
 
 int main() {
     DEBUG = true;
-    Graphe g1{
-        std::vector<Arete*>{},
-        std::vector<const Sommet*>{}
-    };
+    Graphe g1{ std::vector<Arete*>{}, std::vector<const Sommet*>{} };
     Sommet* a = g1.ajouteSommet("A");
     Sommet* b = g1.ajouteSommet("B");
     Sommet c{"C"};
@@ -24,10 +21,7 @@ int main() {
     g1.ajouteArete(&a2);
     g1.symetrise();
     std::cout << g1;
-    Graphe g2{
-        std::vector<Arete*>{},
-        std::vector<const Sommet*>{}
-    };
+    Graphe g2{ std::vector<Arete*>{}, std::vector<const Sommet*>{} };
     g2.ajouteArete(&a1);
     g2.ajouteArete(new Arete{ap, &e, -2});
     std::cout << g2;
