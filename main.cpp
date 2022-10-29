4a8cda3d2c44d790b06f74bf26f5c9ae0f729e95
mazelet <flomazelet@gmail.com>
2022-10-29 17:15:21 +0200

    tests

--- main.cpp
+++ main.cpp
@@ -20,11 +20,24 @@ int main() {
     Edge* e2 = graph1.addEdge(a0, b0, 1);
     graph1.addEdge(e1);
     graph1.addEdge(e2);
+    {
     Graph graph2{ std::vector<Edge*>{}, std::vector<Vertex*>{} };
     graph2.addEdge(a0, b0, 1);
     graph2.addEdge(a0, b0, 2);
-    graph2.addEdge(a0, c, -1);
-    std::cout << graph1;
+    graph2.addVertex(c);
+    graph2.addVertex("X");
+    graph2.addEdge(e1);
+
     std::cout << graph2;    
+
+
+
+    }
+    std::cout << graph1;
+    
     Gc::affiche();
+
+
+
+
 }
